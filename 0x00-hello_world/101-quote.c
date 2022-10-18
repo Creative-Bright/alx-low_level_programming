285aed2c15a9954cc23d39586d3579e82aa51c8f
Creative-Bright <brightbent1@gmal.com>
2022-10-17 21:39:15 -0700

    error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - print without the printf or puts functions
  * printing error function
- * Return: 1
+ * Return: 1 on error
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n, 59");
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
