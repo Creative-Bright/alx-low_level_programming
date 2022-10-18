d3efec49b63e687f5d17b37c89838ec28bdedb79
Creative-Bright <brightbent1@gmal.com>
2022-10-17 17:14:26 -0700

    Adding Readme to hello world directory

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main- this is the beginning of the C programming
+ * function: puts prints messages on the screen
+ * Return: 0 that everything is correct
+ */
+int main(void)
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
