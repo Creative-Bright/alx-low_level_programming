ca001bd6ce5bd54e5934137333da963709be3dc9
Creative-Bright <brightbent1@gmal.com>
2022-10-17 18:24:22 -0700

    My first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main- this is the beginning of the C programming
  * function: puts prints messages on the screen
- * Return: 0 that everything is correct
+ * Return: always 0 that everything is correct
  */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
