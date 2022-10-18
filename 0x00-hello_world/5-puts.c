ec697a626c94ac5187e2d32a87645a55e4089b6e
Creative-Bright <brightbent1@gmal.com>
2022-10-17 18:43:22 -0700

    update on printf

--- 0x00-hello_world/5-puts.c
+++ 0x00-hello_world/5-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - to use the printf command
- * function: what the printf does
- * return: always 0 successful
+ * main - print a c programme
+ *
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art.\n");
 	return (0);
 }
