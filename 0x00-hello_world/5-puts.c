8d30b850dc1202939a3697146c9b062ae00928fc
Creative-Bright <brightbent1@gmal.com>
2022-10-17 18:28:52 -0700

    My newline in c

---
+++ 0x00-hello_world/5-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-to use the printf command
+ * return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
